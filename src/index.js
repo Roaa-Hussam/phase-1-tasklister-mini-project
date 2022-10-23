c947d38d25545b577d087643f0c01cd9b58d52e2
Roaa-Hussam <roaahussam911@gmail.com>
2022-10-23 22:11:25 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  let form = document.querySelector("form");
+  let tasks = document.querySelector("#tasks");
+  let item = document.querySelector("input[type=text]")
+
+
+
+  function test(event) {
+
+    event.preventDefault()
+
+    let li = document.createElement('li');
+    let btn = document.createElement('button');
+    btn.addEventListener('click', deleteItem);
+    btn.textContent = "x";
+    li.textContent = item.value;
+    li.appendChild(btn);
+    tasks.appendChild(li);
+
+    form.reset();
+  }
+
+  form.addEventListener('submit', test)
+
+  function deleteItem(e) {
+
+    e.target.parentNode.remove();
+  }
+
 });
